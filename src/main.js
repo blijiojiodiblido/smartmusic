8906c059f85f83282279d7553f4664f149c36745
blijiojiodiblido <ismartgoo@gmail.com>
2018-04-26 19:25:45 +0800

    全局路由导航守卫。

--- src/main.js
+++ src/main.js
@@ -22,14 +22,36 @@ Vue.use(VueLazyload, {
 })
 
 const historySongs = loadPlay()
-processSongsUrl(historySongs).then((songs) => {
+processSongsUrl(historySongs).then(songs => {
   store.commit(SET_PLAY_HISTORY, songs)
 })
 
 const favoriteSongs = loadFavorite()
-processSongsUrl(favoriteSongs).then((songs) => {
+processSongsUrl(favoriteSongs).then(songs => {
   store.commit(SET_FAVORITE_LIST, songs)
 })
+
+/** ↓↓↓↓↓路由导航全局守卫↓↓↓↓↓ **/
+
+let userStatusConfig = 1
+const USER_LOGIN_STATUS = 1
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/user') {
+    if (userStatusConfig !== USER_LOGIN_STATUS) {
+      console.log('已检测用户登陆状态，此时用户的用户状态为：未登陆，并返回首页。')
+      next({ path: '/' })
+    } else {
+      console.log('已检测用户登陆状态，此时用户的登陆状态为：已登陆')
+      next()
+    }
+  } else {
+    console.log('未检测用户登陆状态，此时用户的登陆状态为：未知')
+    next()
+  }
+})
+
+/** ↑↑↑↑↑路由导航全局守卫↑↑↑↑↑ **/
 
 /* eslint-disable no-new */
 new Vue({
