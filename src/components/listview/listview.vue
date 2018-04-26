cbf563703a85d71a74bfdfbdb5a403e455185f3b
blijiojiodiblido <ismartgoo@gmail.com>
2018-04-26 14:39:34 +0800

    添加注释，便于理解。

--- src/components/listview/listview.vue
+++ src/components/listview/listview.vue
@@ -71,17 +71,23 @@ export default {
     selectItem(item) {
       this.$emit('select', item)
     },
+    // html5的touchstart事件
     onShortcutTouchStart(e) {
+      // 点触目标（锚点）的索引
       let anchorIndex = getData(e.target, 'index')
-      let firstTouch = e.touches[0]
+      // 点触时的位置
+      let firstTouch = e.touches[0] // touches是记录的触摸的点数，一个点为一个对象
       this.touch.y1 = firstTouch.pageY
       this.touch.anchorIndex = anchorIndex
 
       this._scrollTo(anchorIndex)
     },
+    // html5的touchmove事件
     onShortcutTouchMove(e) {
+      // 触摸移动过程中的位置
       let firstTouch = e.touches[0]
       this.touch.y2 = firstTouch.pageY
+      // 手指在屏幕Y轴上的偏移量。
       let delta = ((this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT) | 0
       let anchorIndex = parseInt(this.touch.anchorIndex) + delta
 
@@ -93,6 +99,7 @@ export default {
     scroll(pos) {
       this.scrollY = pos.y
     },
+    // 计算初始化时每个首字母标签块的顶部和底部距离父容器顶部的距离。如：热门块儿的上高度为listHeight[0]，下高度为listHeight[1]。A字母块儿的上高度为listHeight[1]，下高度为listHeight[2]
     _calculateHeight() {
       this.listHeight = []
       const list = this.$refs.listGroup
@@ -113,7 +120,7 @@ export default {
       } else if (index > this.listHeight.length - 2) {
         index = this.listHeight.length - 2
       }
-      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0) // 第二个参数代表滚动是否存在动画。
       this.scrollY = this.$refs.listview.scroll.y
     }
   },
@@ -125,29 +132,37 @@ export default {
     },
     scrollY(newY) {
       const listHeight = this.listHeight
-      // 当滚动到顶部，newY>0
+      // 当滚动到顶部，再往下拉，则newY > 0，currentIndex始终为0。
       if (newY > 0) {
         this.currentIndex = 0
         return
       }
       // 在中间部分滚动
+      // 循环到数组（listHeight）长度减“1”为止是因为数组的值为歌手列表的上下高度值，数组（listHeight）长度比歌手列表长度多“1”
       for (let i = 0; i < listHeight.length - 1; i++) {
+        // 首字母标签块的顶部高度值
         let height1 = listHeight[i]
+        // 首字母标签块的底部高度值
         let height2 = listHeight[i + 1]
+        // 如果新的scrollY值在字母块的height1与height2之间，则currentIndex为该字母块的索引值。
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i
           this.diff = height2 + newY
           return
         }
       }
-      // 当滚动到底部，且-newY大于最后一个元素的上限
+      // 当滚动到页面底部，再往上拉，则-newY大于最后一个元素的底部高度值，currentIndex始终为最后一个字母块的索引值。
       this.currentIndex = listHeight.length - 2
     },
     diff(newVal) {
+      // 页面初始化时，此函数不会执行，滚动页面才会执行此函数。
+      // 页面初始化后第一次开始滚动之时，“newVal”远远大于“TITLE_HEIGHT”，此时fixedTop的值为“0”
       let fixedTop = newVal > 0 && newVal < TITLE_HEIGHT ? newVal - TITLE_HEIGHT : 0
       if (this.fixedTop === fixedTop) {
+        // diff第一次发生变化时，“this.fixedTop” 还未定义，return语句不会执行，执行后面的赋值语句
         return
       }
+      // “fixedTop”的值不为“0”时，“this.fixedTop”的值会随着“fixedTop”的变化而变化
       this.fixedTop = fixedTop
       this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
