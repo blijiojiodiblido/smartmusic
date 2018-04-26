9675c6712bfbd6e1693f35dc4be1d667d2fdbe31
blijiojiodiblido <ismartgoo@gmail.com>
2018-04-26 13:28:12 +0800

    给apply()方法的运用添加注释。

--- src/components/scroll/scroll.vue
+++ src/components/scroll/scroll.vue
@@ -91,10 +91,17 @@ export default {
       this.scroll && this.scroll.refresh()
     },
     scrollTo() {
+      // 在JavaScript中，函数就是一个对象，函数在没有返回值时默认返回的一个对象。
+      // 当一个对象A（this object）没有某个方法，但是其他对象B（other object）有这一方法，我们可以借助call()方法或apply()方法，用其它对象B（other object）的方法来操作。
+      // apply()方法与call()方法的第一个参数是对象A（this object），第二个参数为传递给对象B（other object）的该方法的参数。
+      // 形式为：B.fn.apply(A,[arguments])或者B.fn.call(A,arg1,arg2,arg3……)
+      // 本例中scrollTo方法运行的上下文并未改变，只是传递了参数。因为传递的参数个数并不明确，因此使用apply()方法更加妥当。
       this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      // this.scroll && this.scroll.scrollTo(args)
     },
     scrollToElement() {
       this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      // this.scroll && this.scroll.scrollToElement(args)
     }
   },
   watch: {
